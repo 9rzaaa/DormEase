de5086969c708263fb4cd2129d6568b738ffd76d
amiliadanielle <amiliadanielle06@gmail.com>
2026-06-11 23:45:44 +0800

    fix(notifications): update notification click handling for improved functionality

--- resources/views/frontdeskdb.blade.php
+++ resources/views/frontdeskdb.blade.php
@@ -649,7 +649,7 @@
                         };
                     @endphp
                     <div class="notif-item"
-                         onclick="openNotifDetail(this)"
+                         onclick="handleNotifClick(event, this)"
                          data-notif='{!! json_encode([
                              "id"      => $notif->notif_id,
                              "type"    => $notifTypeLabel,
