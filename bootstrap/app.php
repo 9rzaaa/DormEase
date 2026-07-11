2a917715e9b48140fca8137930211a820ef4a87c
9rzaaa <lingdaniel999@gmail.com>
2026-07-11 19:17:57 +0800

    refactor(middleware): add CSRF token validation for Paymongo webhook

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+            'api/webhooks/paymongo',
+        ]);
         $middleware->trustProxies(at: '*');
         $middleware->alias([
             'dormhead'               => \App\Http\Middleware\DormHeadOnly::class,
